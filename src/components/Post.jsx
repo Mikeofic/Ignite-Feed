870cd97e0479f3f5326fe3ddaf343801bfa518e9
Michel Santos <46272791+Mikeofic@users.noreply.github.com>
2022-09-03 15:56:34 -0300

    validando preenchimento de formulário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -36,6 +36,12 @@ export function Post({ author, publishedAt, content }) {
 
   function handleNewCommentChange() {
     setNewCommentText(event.target.value)
+    event.target.setCustomValidity('')
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity("Esse campo é obrigatório!")
+    
   }
 
   function deleteComment(commentToDelete){
@@ -45,6 +51,8 @@ export function Post({ author, publishedAt, content }) {
 
     setComments(commentsWithoutDeletedOne)
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0
 
   return (
     <article className={styles.post}>
@@ -80,10 +88,12 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           onChange={handleNewCommentChange}
           value={newCommentText}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
         </footer>
       </form>
 
